5b63426d04303ef74b9e1c41d29289aef5a944b9
Valeria91818 <natalalukina00@gmail.com>
2025-05-28 15:38:55 +0400

    lab1.py

--- lab1.py
+++ lab1.py
@@ -1,12 +1,10 @@
-with open("txt.txt", 'r') as f:
-    stroki = f.readline() 
-numbers = stroki.split()
-flag = 0
+with open("txt.txt",'r', encoding='utf-8') as f:
+    stroki=f.readline()
+numbers=stroki.split(',')  
+flag=0
 def num_to_words(digits):
-    digit_words = {
-        '0': 'íîëü', '1': 'îäèí', '2': 'äâà', '3': 'òðè', '4': '÷åòûðå', '5': 'ïÿòü', '6': 'øåñòü', '7': 'ñåìü', '8': 'âîñåìü', '9': 'äåâÿòü'
-    }
-    return ' '.join(digit_words.get(digit, "íåèçâåñòíî") for digit in digits)
+    digit_words = {''0': 'ноль', '1': 'один', '2': 'два', '3': 'три', '4': 'четыре', '5': 'пять', '6': 'шесть', '7': 'семь', '8': 'восемь', '9': 'девять'}
+    return digit_words.get(digits, "неизвестно") 
 for num in numbers:
     if num.isdigit() and len(num) >= 2:
         if int(num) % 2 == 0:
